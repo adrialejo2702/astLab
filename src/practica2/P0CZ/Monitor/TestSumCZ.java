920ffee25e7fa794b907dd1ae77b850ddae96c60
Adrián Alejo Quintero Pedrozo <adrialejo2702@gmail.com>
2021-03-25 20:05:14 +0100

    P0CZ done.

--- src/practica2/P0CZ/Monitor/TestSumCZ.java
+++ src/practica2/P0CZ/Monitor/TestSumCZ.java
@@ -3,6 +3,16 @@ package practica2.P0CZ.Monitor;
 public class TestSumCZ {
 
     public static void main(String[] args) throws InterruptedException {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        MonitorCZ monitorCZ = new MonitorCZ();
+        CounterThreadCZ counterThreadCZ1 = new CounterThreadCZ(monitorCZ);
+        CounterThreadCZ counterThreadCZ2 = new CounterThreadCZ(monitorCZ);
+
+        counterThreadCZ1.start();
+        counterThreadCZ2.start();
+
+        counterThreadCZ1.join();
+        counterThreadCZ2.join();
+
+        System.out.println("Final value of x: "+ monitorCZ.getX());
     }
 }
--- src/practica2/P0CZ/TestSum.java
+++ src/practica2/P0CZ/TestSum.java
@@ -3,7 +3,17 @@ package practica2.P0CZ;
 public class TestSum {
 
     public static void main(String[] args) throws InterruptedException {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        CounterThread counterThread1 = new CounterThread();
+        CounterThread counterThread2 = new CounterThread();
+
+        counterThread1.start();
+        counterThread2.start();
+
+        counterThread1.join();
+        counterThread2.join();
+
+        System.out.println("Final value of x: "+ CounterThread.x);
+
     }
 }
 
